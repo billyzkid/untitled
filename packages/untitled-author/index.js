0c7dc230bcd89a8f392b9fd034d9fee99a317115
Will Allan <billyzkid@yahoo.com>
2017-02-08 01:23:23 -0800

    Lint

--- packages/untitled-author/index.js
+++ packages/untitled-author/index.js
@@ -2,13 +2,7 @@
 
 "use strict";
 
-var chalk = require("chalk");
-var childProcess = require("child_process");
-var commander = require("commander");
-var crossSpawn = require("cross-spawn");
-var fsExtra = require("fs-extra");
 var packageJson = require("./package.json");
-var path = require("path");
-var semver = require("semver");
 
+// eslint-disable-next-line no-console
 console.warn(`${packageJson.name} is not implemented!`);
