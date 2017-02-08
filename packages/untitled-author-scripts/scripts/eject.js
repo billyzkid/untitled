1f17a3273bc44c2d40aa7e8d295d19a8928dae6e
Will Allan <billyzkid@yahoo.com>
2017-02-08 02:26:17 -0800

    Added scripts

---
+++ packages/untitled-author-scripts/scripts/eject.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var packageJson = require("./package.json");
+
+// eslint-disable-next-line no-console
+console.warn(`${packageJson.name} is not implemented!`);
---
+++ packages/untitled-author-scripts/template/src/index.js
@@ -0,0 +1,17 @@
+import { Story } from "untitled";
+import { English } from "untitled-grammars";
+import { Automobile } from "untitled-vehicles";
+
+const myStory = new Story();
+myStory.grammar = new English();
+myStory.title = "My Story";
+
+const driveway = new Place();
+driveway.name = "driveway";
+myStory.children.push(driveway);
+
+const car = new Automobile();
+car.name = "car";
+driveway.children.push(car);
+
+export default myStory;
