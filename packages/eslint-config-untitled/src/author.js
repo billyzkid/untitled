f4082b8a6e59336a248ff12bbb1ab44228430aa1
Will Allan <billyzkid@yahoo.com>
2017-02-10 17:05:28 -0800

    Use root .babelrc and .eslintrc

--- packages/eslint-config-untitled/src/author.js
+++ packages/eslint-config-untitled/src/author.js
@@ -1,5 +1,5 @@
-// NOTE: Due to a change in Babel 6, "export default" will not work here
-// See https://babeljs.io/docs/plugins/transform-es2015-modules-commonjs/
+// NOTE: Due to a change in Babel 6, "export default" will not work here:
+// https://babeljs.io/docs/plugins/transform-es2015-modules-commonjs/
 
 module.exports = {
   root: "true",
