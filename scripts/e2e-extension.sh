345d2684c440b900b6d059687aedfbdddb46bfe4
Will Allan <will.allan@mlb.com>
2017-02-13 15:53:23 -0800

    Migrated nuke script to node

--- scripts/e2e-extension.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# See https://github.com/facebookincubator/create-react-app/blob/master/tasks/cra.sh
-# https://github.com/facebookincubator/create-react-app/blob/master/tasks/e2e-kitchensink.sh
---
+++ scripts/nuke.js
@@ -0,0 +1,10 @@
+const rimraf = require("rimraf");
+
+process.chdir(__dirname);
+
+rimraf.sync("../packages/*/build");
+rimraf.sync("../packages/*/coverage");
+rimraf.sync("../packages/*/node_modules");
+rimraf.sync("../packages/*/*.log");
+rimraf.sync("../node_modules");
+rimraf.sync("../*.log");
