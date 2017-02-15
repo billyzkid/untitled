b08a2ceedb2be035e223328addd4ae6ba0e341d2
Will Allan <billyzkid@yahoo.com>
2017-02-15 01:27:54 -0800

    Added tests

--- packages/untitled-cli/src/index.test.js
+++ packages/untitled-cli/src/index.test.js
@@ -1,5 +1,12 @@
-describe("untitled-cli", () => {
-  test("should test something", () => {
-    expect(undefined).toBeUndefined();
+import { execute } from "./index"
+
+describe("execute", () => {
+  test("should be an exported function", () => {
+    expect(execute).toBeInstanceOf(Function);
+  });
+
+  test("should succeed", () => {
+    const code = execute();
+    expect(code).toBe(0);
   });
 });
--- packages/untitled-scripts/src/index.test.js
+++ packages/untitled-scripts/src/index.test.js
@@ -1,5 +1,12 @@
-describe("untitled-scripts", () => {
-  test("should test something", () => {
-    expect(undefined).toBeUndefined();
+import { execute } from "./index"
+
+describe("execute", () => {
+  test("should be an exported function", () => {
+    expect(execute).toBeInstanceOf(Function);
+  });
+
+  test("should succeed", () => {
+    const code = execute("blah", ["blech"]);
+    expect(code).toBe(0);
   });
 });
