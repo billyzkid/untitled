c268afe63eacc6a7358be6bc5e0357b8f91fafbd
Will Allan <billyzkid@yahoo.com>
2017-02-24 12:39:41 -0800

    Better scripts

--- scripts/authors.js
+++ scripts/authors.js
@@ -1,81 +1,19 @@
-const fetch = require("node-fetch");
 const fs = require("fs");
+const github = require("./github");
 const minimist = require("minimist");
 const packageJson = require("../package.json");
 
-const apiEndpoint = "https://api.github.com";
-const apiToken = process.env.GITHUB_API_TOKEN;
-const userAgent = packageJson.name;
-const repo = packageJson.repository;
-
-function request(method, url) {
-  if (!url.startsWith(apiEndpoint)) {
-    url = apiEndpoint + url;
+const options = minimist(process.argv.slice(2), {
+  default: {
+    "repo": packageJson.repository,
+    "format": "text",
+    "out-file": ""
   }
+});
 
-  const headers = {
-    "Accept": "application/vnd.github.v3+json",
-    "User-Agent": userAgent
-  };
-
-  if (apiToken) {
-    headers["Authorization"] = `token ${apiToken}`;
-  }
-
-  return fetch(url, { method, headers }).then((response) => {
-    if (!response.ok) {
-      throw new Error(`Request failed with response status ${response.status} (${response.statusText}): ${url}`);
-    }
-
-    return response;
-  });
-}
-
-function get(url, paged, items = []) {
-  return request("GET", url).then((response) => {
-    if (!paged) {
-      return response.json();
-    } else {
-      return response.json().then((data) => {
-        if (data instanceof Array) {
-          items.push(...data);
-        } else if (data.items instanceof Array) {
-          items.push(...data.items);
-        } else {
-          throw new Error("Unexpected response data");
-        }
-
-        const linkHeader = response.headers.get("link");
-        const links = (linkHeader) ? linkHeader.split(", ") : [];
-        const nextUrl = links.reduce((nextLink, link) => {
-          const matches = link.match(/^<(.*)>; rel="(.*)"$/);
-          if (matches && matches[2] === "next") {
-            return matches[1];
-          } else {
-            return nextLink;
-          }
-        }, null);
-
-        if (nextUrl) {
-          return get(nextUrl, paged, items);
-        } else {
-          return items;
-        }
-      });
-    }
-  });
-}
-
-function format(type, data) {
-  switch (type) {
-    case "json":
-      return formatJson(data);
-    case "text":
-      return formatText(data);
-    default:
-      throw new Error(`Unknown format: ${type}`);
-  }
-}
+const repo = options.repo;
+const format = options.format;
+const outputFile = options["out-file"];
 
 function formatJson(data) {
   return JSON.stringify(data, null, 2);
@@ -99,20 +37,21 @@ function formatText(data) {
   }).sort().join("\n");
 }
 
-const options = minimist(process.argv.slice(2), {
-  default: {
-    "format": "text",
-    "out-file": ""
-  }
-});
-
-get(`/repos/${repo}/contributors`, true).then((data) => {
-  const promises = data.filter((c) => c.url).map((c) => get(c.url).then((u) => c.user = u));
+github.get(`/repos/${repo}/contributors`, true).then((data) => {
+  const promises = data.filter((c) => c.url).map((c) => github.get(c.url).then((u) => c.user = u));
   return Promise.all(promises).then(() => data);
 }).then((data) => {
-  const output = format(options.format, data);
-  if (options["out-file"]) {
-    fs.writeFileSync(options["out-file"], output);
+  switch (format) {
+    case "json":
+      return formatJson(data);
+    case "text":
+      return formatText(data);
+    default:
+      throw new Error(`Unknown format: ${format}`);
+  }
+}).then((output) => {
+  if (outputFile) {
+    fs.writeFileSync(outputFile, output);
   } else {
     console.log(output);
   }
---
+++ scripts/github.js
@@ -0,0 +1,67 @@
+const fetch = require("node-fetch");
+const packageJson = require("../package.json");
+
+const apiEndpoint = "https://api.github.com";
+const apiToken = process.env.GITHUB_API_TOKEN;
+
+function request(method, url, pagedData) {
+  if (!url.startsWith(apiEndpoint)) {
+    url = apiEndpoint + url;
+  }
+
+  const headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "User-Agent": `${packageJson.name}/${packageJson.version}`
+  };
+
+  if (apiToken) {
+    headers["Authorization"] = `token ${apiToken}`;
+  }
+
+  return fetch(url, { method, headers }).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Request failed with response status ${response.status} (${response.statusText}): ${url}`);
+    }
+
+    if (pagedData) {
+      return response.json().then((data) => {
+        if (data instanceof Array) {
+          pagedData.push(...data);
+        } else if (data.items instanceof Array) {
+          pagedData.push(...data.items);
+        } else {
+          throw new Error("Unexpected response data");
+        }
+
+        const linkHeader = response.headers.get("link");
+        const links = (linkHeader) ? linkHeader.split(", ") : [];
+        const nextUrl = links.reduce((nextLink, link) => {
+          const linkMatch = link.match(/^<(.*)>; rel="(.*)"$/);
+          if (linkMatch && linkMatch[2] === "next") {
+            return linkMatch[1];
+          } else {
+            return nextLink;
+          }
+        }, null);
+
+        if (nextUrl) {
+          return request(method, nextUrl, pagedData);
+        } else {
+          return pagedData;
+        }
+      });
+    }
+
+    return response.json();
+  });
+}
+
+function get(url, paged) {
+  if (paged) {
+    return request("GET", url, []);
+  } else {
+    return request("GET", url);
+  }
+}
+
+exports.get = get;
